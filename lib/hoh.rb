c2d64f4553356b421ee4726f1e5a632c1a830671
Melike KILIC <melike180400@gmail.com>
2020-02-20 17:36:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,19 +47,19 @@ def naming_system
      label: "Phylum",
      sub_category: {
        label: "Class",
-       sub_category  {
+       sub_category:  {
          label: "Order",
-         sub_category  {
+         sub_category:  {
            label: "Family",
-           sub_category {
+           sub_category: {
              label: "Genus",
-             sub_category {
+             sub_category: {
                label: "Species",
                 sub_category: nil
                
                     
               }
-        
+           }
          }
        }
      }
